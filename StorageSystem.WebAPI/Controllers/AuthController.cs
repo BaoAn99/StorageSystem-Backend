2a4458731422b82c37fc5471752d36a64b027463
BaoAn99 <huynh.baoan1999@gmail.com>
2023-08-14 18:16:38 +0700

    create jwt

---
+++ StorageSystem.WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.IdentityModel.Tokens;
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace StorageSystem.WebAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly IConfiguration configuration;
+        public AuthController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        [HttpPost]
+        public IActionResult Authenticate([FromBody] Credential credential)
+        {
+            Console.WriteLine("vao");
+            if(credential.UserName == "admin" && credential.Password == "admin123") {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email, "admin123@gmail.com"),
+                    new Claim("Departmet", "HR"),
+                    new Claim("Admin", "true"),
+                    new Claim("Manager", "true"),
+                    new Claim("EmployeeeDate", "2023-08-14")
+                };
+
+                var expireAt = DateTime.UtcNow.AddMinutes(10);
+
+                return Ok(new { access_tokten = CreateToken(claims, expireAt), expires_at = expireAt });
+                
+            }
+            ModelState.AddModelError("Unauthorized", "you are not authorize to access the endpoint");
+
+            return Unauthorized(ModelState);
+        }
+
+        private string CreateToken(IEnumerable<Claim> claims, DateTime expireAt)
+        {
+            var secretKey = Encoding.ASCII.GetBytes(configuration.GetValue<string>("SecretKey"));
+
+            var jwt = new JwtSecurityToken(
+                claims: claims,
+                notBefore: DateTime.UtcNow,
+                expires: expireAt,
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(secretKey), SecurityAlgorithms.HmacSha256Signature));
+
+            return new JwtSecurityTokenHandler().WriteToken(jwt);
+        }
+
+
+    }
+
+    public class Credential
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }    
+    }
+}
---
+++ StorageSystem.WebAPI/Program.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using StorageSystem.Application;
+using StorageSystem.EntityFrameworkCore.EntityFrameworkCore;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddAutoMapper(c => c.AddProfile<AutoMapping>(), typeof(Program));
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(
+            builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration.GetValue<string>("SecretKey"))),
+        ValidateLifetime = true,
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddAuthorization();
+
+//Đăng ký Identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options => {
+    // Thiết lập về Password
+    //options.Password.RequireDigit = false; // Không bắt phải có số
+    //options.Password.RequireLowercase = false; // Không bắt phải có chữ thường
+    //options.Password.RequireNonAlphanumeric = false; // Không bắt ký tự đặc biệt
+    //options.Password.RequireUppercase = false; // Không bắt buộc chữ in
+    //options.Password.RequiredLength = 3; // Số ký tự tối thiểu của password
+    //options.Password.RequiredUniqueChars = 1; // Số ký tự riêng biệt
+
+    //// Cấu hình Lockout - khóa user
+    //options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(1); // Khóa 1 phút
+    //options.Lockout.MaxFailedAccessAttempts = 3; // Thất bại 5 lầ thì khóa
+    //options.Lockout.AllowedForNewUsers = true;
+
+    //// Cấu hình về User.
+    //options.User.AllowedUserNameCharacters = // các ký tự đặt tên user
+    //    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    //options.User.RequireUniqueEmail = true; // Email là duy nhất
+
+    //// Cấu hình đăng nhập.
+    //options.SignIn.RequireConfirmedEmail = true; // Cấu hình xác thực địa chỉ email (email phải tồn tại)
+    //options.SignIn.RequireConfirmedPhoneNumber = false; // Xác thực số điện thoại
+
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
