89dc0062e4be2f32c24a0b5602a248ba3157023e
DESKTOP-QGTE358\ASUS <phamduykhuong1412@gmail.com>
2023-12-29 18:30:55 +0700

    update create product

--- StorageSystem.Application/Models/Product/Base/CreateOrUpdateProductDto.cs
+++ StorageSystem.Application/Models/Product/Base/CreateOrUpdateProductDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StorageSystem.Application.Models.Products.Base
+﻿namespace StorageSystem.Application.Models.Products.Base
 {
     public class CreateOrUpdateProductDto
     {
@@ -21,5 +15,9 @@ namespace StorageSystem.Application.Models.Products.Base
         public string? Description { set; get; }
         
         public Guid CategoryId { get; set; }
+
+        public string ThumbnailImage { get; set; }
+
+        public virtual List<Domain.Entities.ProductImage> ProductImages { get; set; }
     }
 }
