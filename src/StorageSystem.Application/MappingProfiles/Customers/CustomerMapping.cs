947115b643def019858ae0792d0759a71f7bc7db
Duong Minh Trieu <trieu7458@gmail.com>
2024-10-09 16:10:53 +0700

    crud customer

---
+++ src/StorageSystem.Application/MappingProfiles/Customers/CustomerMapping.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using StorageSystem.Application.Models.Customer;
+using StorageSystem.Domain.Entities.Customers;
+
+namespace StorageSystem.Application.MappingProfiles.Customers
+{
+    public class CustomerMapping : Profile
+    {
+        public CustomerMapping()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            CreateMap<CustomerCreateDto, Customer>();
+            CreateMap<CustomerUpdateDto, Customer>();
+            //CreateMap<Customer, CustomerForView>();
+
+        }
+    }
+}
